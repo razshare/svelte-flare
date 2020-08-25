c0c19207a7e85313b7d506ad801b159bc1e57067
Razvan <tangent.jotey@gmail.com>
2020-08-25 08:38:27 +0200

    update

--- src/pages/HomePage.svelte
+++ src/pages/HomePage.svelte
@@ -11,7 +11,7 @@ $:console.log("Toggle:",value);
 <CheckAnimation />
 <ComputerAnimation>
     <p style="position:absolute;left:0;max-width:100%;top:0;padding: 1rem">
-        You should still be able to select and lcick me through the canvas!
+        You should still be able to select and click me through the canvas!
     </p>
     <MultiOptionButton style="position:absolute;left: 0;right:0;top:0" onclick={(key)=>{
         console.log(key," clicked!");
